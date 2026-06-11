2efd76ea3ce654ffb45a099b5062ec18b8f13fae
Manaswini249 <umanaswini23@gmail.com>
2026-06-11 10:10:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const toggleButton =
+document.getElementById(
+  "theme-toggle"
+);
+
+toggleButton.addEventListener(
+  "click",
+  () => {
+
+    document.body.classList.toggle(
+      "dark-mode"
+    );
+
+  }
+);
